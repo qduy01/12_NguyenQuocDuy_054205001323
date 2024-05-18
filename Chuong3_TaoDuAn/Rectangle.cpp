753ac7c458f2aeb0a992162c4769b090c38dee4f
qduy01 <duygudboy123@gmail.com>
2024-05-18 14:47:07 +0700

    Add files via upload

---
+++ Chuong3_TaoDuAn/Rectangle.cpp
@@ -0,0 +1,23 @@
+#include "Rectangle.h"
+//Rectangle.cpp
+#include "Rectangle.h"
+int CRectangle::getWidth() const
+{
+	return width;
+}
+int CRectangle::getHeigth() const
+{
+	return heigth;
+}
+void CRectangle::setWidth(int _width)
+{
+	width = _width;
+}
+void CRectangle::setHeigth(int _heigth)
+{
+	heigth = _heigth;
+}
+int CRectangle::area()
+{
+	return width * heigth;
+}
